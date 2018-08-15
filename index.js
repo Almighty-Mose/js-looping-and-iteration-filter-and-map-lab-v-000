ae98ed44d009055d7a24762905c7712338ffad31
Branden Mosley <branden.mosley1337@gmail.com>
2018-08-15 19:46:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function driversWithRevenueOver(drivers, revenue) {
 }
 
 function driverNamesWithRevenueOver(drivers, revenue) {
-  return driversWithRevenueOver(drivers, revenue).map {
+  return driversWithRevenueOver(drivers, revenue).map(function {
     return driver.name
   }
 }
